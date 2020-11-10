0006bb1ad925c59f18d8b814d73763d31a8b4eb4
Davide <Sbalzerdavide@hotmail.it>
2020-11-10 16:28:42 +0100

    first version

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ const app = new Vue ({
 
     data: {
         saluto: 'salutone',
+        toDoList:[
+            'Fare la spesa',
+            'Chiamare Giovanni',
+            'Aperitivo',
+            'Controllare Chiarella',
+        ],
+        message:'',
+        
+
+        
 
     },
 
@@ -11,7 +21,29 @@ const app = new Vue ({
     },
 
     methods:{
+        addItem(){
+            console.log('click');
+            if (this.message != ''){
+                this.toDoList.push(this.message);
+            };
+            console.log(this.toDoList);
+            this.message='';
+        },
+        removeItem(index){
+            console.log(index),
+            console.log('removed'),
+            this.toDoList.splice(index,1);
 
+        }
+        
+
+
+
+        // readM(){
+        //     console.log(this.message);
+        // },
     }
+
+    
 
 });
