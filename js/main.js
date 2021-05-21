983c75d997b7bfb3431b66c8823438bd58901157
DavideS <davides@edm.locale>
2021-05-21 16:33:21 +0200

    sw 1

--- js/main.js
+++ js/main.js
@@ -77,3 +77,12 @@ const app = new Vue ({
     
 
 });
+
+// Handle incoming messages. Called when:
+// - a message is received while the app has focus
+// - the user clicks on an app notification created by a service worker
+//   `messaging.onBackgroundMessage` handler.
+messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+    // ...
+});
--- js/sw.js
+++ js/sw.js
@@ -35,11 +35,3 @@ messaging.onBackgroundMessage((payload) => {
       notificationOptions);
   });
 
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.onBackgroundMessage` handler.
-messaging.onMessage((payload) => {
-    console.log('Message received. ', payload);
-    // ...
-});
