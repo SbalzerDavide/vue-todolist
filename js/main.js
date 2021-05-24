d6ac2fef3c5b87de34a69f3e1fc84a8071ba216d
DavideS <davides@edm.locale>
2021-05-24 09:30:58 +0200

    change path

--- js/main.js
+++ js/main.js
@@ -22,17 +22,17 @@ const app = new Vue ({
     },
     created(){
           
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', function() {
-              navigator.serviceWorker.register('./js/sw.js').then(function(registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-              }, function(err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-              });
-          });
-        }
+        // if ('serviceWorker' in navigator) {
+        //     window.addEventListener('load', function() {
+        //       navigator.serviceWorker.register('./js/sw.js').then(function(registration) {
+        //         // Registration was successful
+        //         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        //       }, function(err) {
+        //         // registration failed :(
+        //         console.log('ServiceWorker registration failed: ', err);
+        //       });
+        //   });
+        // }
         // localStorage.setItem("todo", this.toDoList);
         let localStor = localStorage.getItem("todo")
         if ( localStor != null){
